a9194a693fd747d02dfc0e2eb3e5db86890e0426
zuozuo <c_yzuo@groupon.com>
2017-05-07 17:57:20 +0800

    config/database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "3.8.1"
 
 set :application, "gaoxiangshu"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:zuozuo/gaoxiangshu.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/home/deploy/webapp/gaoxiangshu'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,10 +21,10 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+server '118.190.3.38', user: 'deploy', roles: %w{app db web}
+
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
